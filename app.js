34aa379e356cd110db6444b897db7873103119fd
Hannes Rosenbauer <hannes.rosenbauer@gmail.com>
2014-03-23 20:04:13 +0100

    use bower, jquery, bootstrap

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.engine('ejs', ejs);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 // configure express
 app.use(express.json());
